87a331978ae1dcbf01bae7116c26cdeec362a6b1
2nozha <Anna-Seslavinskaya@mail.ru>
2021-10-18 13:15:46 +0300

    small refactor and comments added

--- src/main/java/ru/dataart/academy/java/Calculator.java
+++ src/main/java/ru/dataart/academy/java/Calculator.java
@@ -12,9 +12,12 @@ public class Calculator {
         //Task implementation
         return getValue(firstNumber)+getValue(secondNumber);
     }
+    //function reverses and writes together all array numbers
     private Integer getValue(List<Integer> array){
         Integer value=0;
+        //iterator with lists' size as initial position
         ListIterator<Integer> iterator = array.listIterator(array.size());
+        //going through array backwards
         while (iterator.hasPrevious()) {
             value=value*10+iterator.previous();
         }
@@ -32,8 +35,10 @@ public class Calculator {
         // Task implementation
         List<T> resultList =new ArrayList<>();
         Iterator iterator = list.iterator();
+        //going through lists' elements
         while (iterator.hasNext()){
             resultList.add((T)iterator.next());
+            //construction, which allows skipping (without wrighting in result set) element after one written above
             if(iterator.hasNext()){
                 iterator.next();
             }
@@ -52,18 +57,19 @@ public class Calculator {
     public <T> List<T> getBounds(List<T> list) {
         // Task implementation
         List<T> resultList =new ArrayList<>();
-        Iterator iterator = list.listIterator();
-        if (iterator.hasNext()){
-            resultList.add((T)iterator.next());
-            if (iterator.hasNext()){
-                ListIterator iterator1 = list.listIterator(list.size());
-                if (iterator1.hasPrevious()){
-                    resultList.add((T)iterator1.previous());
-                }
+        //iterator with default start position
+        Iterator startIterator = list.iterator();
+        //if list is no empty, writing first element
+        if (startIterator.hasNext()){
+            resultList.add((T)startIterator.next());
+            //if list contains more than one element
+            if (startIterator.hasNext()){
+                //iterator with initial position at the end of list
+                ListIterator endIterator = list.listIterator(list.size());
+                //writing last element
+                resultList.add((T)endIterator.previous());
             }
         }
-
-
         return resultList;
     }
 }
